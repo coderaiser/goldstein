0d997c343f02bbce9f3024e9694491f7470891db
coderaiser <coderaiser@cloudcmd.io>
2024-02-20 12:10:19 +0200

    feature: goldstein: printer: visitors: await-expression: maybeVisitor

--- packages/printer/visitors/await-expression.js
+++ packages/printer/visitors/await-expression.js
@@ -1,10 +1,13 @@
-import {visitors as v} from '@putout/printer';
+import {
+    maybeVisitor,
+    visitors as v,
+} from '@putout/printer';
 
 export const AwaitExpression = (path, printer, semantics) => {
     const {print} = printer;
     
     if (!path.node.goldstein)
-        return v.AwaitExpression(path, printer, semantics);
+        return maybeVisitor(v.AwaitExpression, path, printer, semantics);
     
     print('__goldstein');
 };
---
+++ packages/printer/visitors/await-expression.spec.js
@@ -0,0 +1,18 @@
+import {test} from 'supertape';
+import montag from 'montag';
+import estreeToBabel from 'estree-to-babel';
+import {print} from '../index.js';
+import {parse} from '../../goldstein/index.js';
+
+test('goldstein: printer: visitors: await', (t) => {
+    const source = `const a = await f('hello')`;
+    const ast = estreeToBabel(parse(source));
+    const result = print(ast);
+    
+    const expected = montag`
+        const a = await f('hello');\n
+    `;
+    
+    t.equal(result, expected);
+    t.end();
+});
