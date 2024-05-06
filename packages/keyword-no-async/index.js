139985431ff558790535938dbff9b2552211b2df
coderaiser <coderaiser@cloudcmd.io>
2024-05-06 19:06:44 +0300

    feature: @putout/plugin-goldstein: c8 v9.1.0

---
+++ packages/keyword-no-async/index.js
@@ -0,0 +1,36 @@
+import {tokTypes as tt} from 'acorn';
+
+export default function fn(Parser) {
+    return class extends Parser {
+        parseBlock(createNewLexicalScope, node, exitStrict) {
+            if (createNewLexicalScope === void 0)
+                createNewLexicalScope = true;
+            
+            if (node === void 0)
+                node = this.startNode();
+            
+            node.body = [];
+            // optionally parse arrow
+            this.eat(tt.arrow);
+            this.expect(tt.braceL);
+            
+            if (createNewLexicalScope)
+                this.enterScope(0);
+            
+            while (this.type !== tt.braceR) {
+                const stmt = this.parseStatement(null);
+                node.body.push(stmt);
+            }
+            
+            if (exitStrict)
+                this.strict = false;
+            
+            this.next();
+            
+            if (createNewLexicalScope)
+                this.exitScope();
+            
+            return this.finishNode(node, 'BlockStatement');
+        }
+    };
+}
