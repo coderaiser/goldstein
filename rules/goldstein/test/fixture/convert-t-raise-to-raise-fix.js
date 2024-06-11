576644c54020916f3e8b051ce45b2f27a80f002a
coderaiser <coderaiser@cloudcmd.io>
2024-06-11 17:25:15 +0300

    chore: update fixtures

--- rules/goldstein/test/fixture/convert-t-raise-to-raise-fix.js
+++ rules/goldstein/test/fixture/convert-t-raise-to-raise-fix.js
@@ -1,3 +1,3 @@
-test('goldstein: keyword: try: not-supported', (t) => {
-    t.raise('not-supported', `After 'try' only '{', 'await' and 'function call' can come (1:8)`);
+test('goldstein: keyword: try: not-supported', async ({raise}) => {
+    await raise('not-supported', `After 'try' only '{', 'await' and 'function call' can come (1:8)`);
 });
