76cb57d629ed0450f87ec031a99f2f2d63e98377
coderiaser <coderaiser@cloudcmd.io>
2024-10-27 20:39:31 +0200

    feature: goldstein: @putout/printer v10.0.1

--- packages/printer/visitors/call-expression.js
+++ packages/printer/visitors/call-expression.js
@@ -1,10 +1,13 @@
-import {visitors as v} from '@putout/printer';
+import {
+    maybeVisitor,
+    visitors as v,
+} from '@putout/printer';
 
 export const CallExpression = (path, printer, semantics) => {
     const {print} = printer;
     
     if (!path.node.goldstein)
-        return v.CallExpression(path, printer, semantics);
+        return maybeVisitor(v.CallExpression, path, printer, semantics);
     
-    print('__goldstein');
+    return print('__goldstein');
 };
