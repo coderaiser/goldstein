ba5b0ce20dbfab52767820cf7f3f0b2f6edb1d64
coderaiser <coderaiser@users.noreply.github.com>
2024-12-19 12:21:45 +0000

    chore: goldstein: actions: lint ☘️

--- packages/keyword-broken-string/index.js
+++ packages/keyword-broken-string/index.js
@@ -60,5 +60,8 @@ export default function keywordBrokenString(Parser) {
 }
 
 function isNewLine(code) {
-    return code === 10 || code === 13 || code === 0x2028 || code === 0x2029;
+    return code === 10
+        || code === 13
+        || code === 0x2028
+        || code === 0x2029;
 }
